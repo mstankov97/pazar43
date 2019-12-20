cf08b9da7fec1446d71fbf2f4b5a82a0e167b414
mstankov97 <mstankov97@gmail.com>
2019-12-20 21:30:36 +0100

    Initial commit

---
+++ src/main/java/com/prostaks/pazar43/Pazar43Application.java
@@ -0,0 +1,13 @@
+package com.prostaks.pazar43;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pazar43Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pazar43Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/prostaks/pazar43/Pazar43ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prostaks.pazar43;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pazar43ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
